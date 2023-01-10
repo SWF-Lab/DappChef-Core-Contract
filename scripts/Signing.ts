985a966ef3d2028553579b415f658a661042d5f7
ChiHaoLu <b08303113@ntu.edu.tw>
2023-01-10 22:36:23 +0800

    revise signing script

--- scripts/Signing.ts
+++ scripts/Signing.ts
@@ -4,17 +4,16 @@ async function main() {
 
     // Prepare the provider & wallet(signer)
     const provider = ethers.provider
-    const walletMnemonic = ethers.Wallet.fromMnemonic(process.env.ETHEREUM_PRIVATE_KEY as string)
-    const wallet = new ethers.Wallet(walletMnemonic.privateKey, provider)
+    const wallet = new ethers.Wallet(process.env.ETHEREUM_PRIVATE_KEY as any, provider)
 
     // The data to sign
     const problemSolverAddr = '0xDEcf23CbB14972F2e9f91Ce30515ee955a124Cba'
     const problemNumber = '997'
     const problemSolvedTimestamp = 1673070083
-    const approverKeyAddr = process.env.CHEF_KEY_ARRR
+    const approverKeyAddr = process.env.DEV_KEY_2_ARRR as string
 
     console.log(
-        `Signer Key Address: ${ethers.utils.computeAddress(wallet.privateKey)}`,
+        `Signer Key Address: ${wallet.address}`,
     )
     console.log(`    - Problem Solver Address  : ${problemSolverAddr}`)
     console.log(`    - Problem Number is       : ${problemNumber}`)
@@ -27,13 +26,21 @@ async function main() {
         [problemSolverAddr, problemNumber, problemSolvedTimestamp, approverKeyAddr]
     )
     const msgHash = ethers.utils.keccak256(encode)
+
+    const messageHash = ethers.utils.solidityKeccak256(["address", "uint256", "uint256", "address"],
+        [problemSolverAddr, problemNumber, problemSolvedTimestamp, approverKeyAddr])
+    const messageHashBinary = ethers.utils.arrayify(messageHash)
+    const signature = await wallet.signMessage(messageHashBinary)
+    const verified = ethers.utils.verifyMessage(messageHashBinary, signature)
+
+
     console.log(`\nSigning Hash: ${msgHash}`)
-    const signature = await wallet.signMessage(msgHash);
     console.log(`Signature: ${signature}`)
 
     // Check the Signature is Valid
-    const valid = ethers.utils.recoverAddress(msgHash, signature) == approverKeyAddr;
-    console.log(`\nCheck the Signature is Valid...${valid ? "Approved!" : "Invalid!"}`)
+
+    const valid = verified == approverKeyAddr;
+    console.log(`\nCheck the Signature is...${valid ? "Approved!" : "Invalid!"}`)
 }
 
 
