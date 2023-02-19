d6d356bc41e2bb54e535b753d1bd831e92183e26
FoodChain1028 <sh1001309@gmail.com>
2023-02-19 13:51:34 +0800

    add utils.ts

--- test/utils.ts
+++ test/utils.ts
@@ -23,4 +23,45 @@ const getSignature = async (
     return signature;
 }
 
-export { getSignature }
+const checkApproverIndex = (address: string) => {
+    if (address === process.env.SERVER_KEY_ADDR) return 0;
+    else if (address === process.env.CHEF_KEY_ADDR) return 1;
+    else if (address === process.env.LAB_KEY_ADDR) return 2;
+    else if (address === process.env.DEV_KEY_1_ADDR) return 3;
+    else if (address === process.env.DEV_KEY_2_ADDR) return 4;
+    else return (-1);
+}
+
+const getMsgHash = (
+    problemSolverAddr: string, 
+    problemNumber: string, 
+    problemSolvedTimestamp: number, 
+    approverKeyAddr: string, 
+    approverIndex: number
+) => {
+    const messageHash = ethers.utils.solidityKeccak256(
+        ["address", "uint256", "uint256", "address", "uint8"],
+        [problemSolverAddr, problemNumber, problemSolvedTimestamp, approverKeyAddr, approverIndex]
+    );
+    return messageHash;
+}
+
+const getEthMsgHash = (
+    problemSolverAddr: string, 
+    problemNumber: string, 
+    problemSolvedTimestamp: number, 
+    approverKeyAddr: string, 
+    approverIndex: number
+) => {
+    const messageHash = getMsgHash(
+        problemSolverAddr, 
+        problemNumber, 
+        problemSolvedTimestamp, 
+        approverKeyAddr, 
+        approverIndex
+    );
+    const signingHash = ethers.utils.solidityKeccak256(["string", "bytes32"], ["\x19Ethereum Signed Message:\n32", messageHash]);
+    return signingHash;
+}
+
+export { getSignature, getMsgHash, getEthMsgHash, checkApproverIndex }
